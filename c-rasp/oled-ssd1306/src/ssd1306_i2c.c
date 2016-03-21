61faec9c134b3dd024e9533a70eeeea650c92e05
Zuo Xiang <xianglinks@gmail.com>
2016-03-21 10:10:01 +0100

    add codes in oled-ssd1306

--- c-rasp/oled-ssd1306/src/ssd1306_i2c.c
+++ c-rasp/oled-ssd1306/src/ssd1306_i2c.c
@@ -16,26 +16,62 @@
 #include <wiringPiI2C.h>
 
 #include "ssd1306_i2c.h"
+#include "font.h"
 
-// send SSD1306 control command using I2C
+// send command byte to SSD1306
 void sendCommand(unsigned char command)
 {
-    // write 8 bits data value into the device register
+    // write 8 bits data value to the given register
     wiringPiI2CWriteReg8(SSD1306_I2C_ADDRESS, SSD1306_CONTROL_REG, command);
 }
 
-// send char to SSD1306
+// send data byte to SSD1306
 void sendChar(unsigned char data)
 {
-    // write 8 bits data value into the device register
+    // write 8 bits data value to the given register
     wiringPiI2CWriteReg8(SSD1306_I2C_ADDRESS, SSD1306_DATA_REG, data);
 }
 
-// init SSD1306 Driver
-int ssd1306I2CSetup(void)
+void setCursorXY(int row, int col)
 {
-    int re;
+    sendCommand(0xB0 + (row & 0x0F));  // set page address, row is controlled by page
+    sendCommand(0x00 + (8 * col & 0x0F) + 0);  // set low col address
+    sendCommand(0x10 + (( 8 * col>>4) & 0x0F));  // set high col address
+}
+
+void sendStrXY(const char *string, int X, int Y)
+{
+    unsigned char i;
+
+    setCursorXY(X, Y);
+    while(*string) {
+      for(i=0; i<8; i++) {
+        sendChar(pgm_read_byte(myFont[*string-0x20] + i));
+      }
+      *string++;
+    }
+}
+
+void displayOn(void)
+{
+    sendCommand(SSD1306_DISPLAYON);
+}
+
+void displayOff(void)
+{
+    sendCommand(SSD1306_DISPLAYOFF);
+}
+
+void clearDisplay(void)
+{
+
+}
+
+// init I2C interface and SSD1306
+void ssd1306I2CSetup(void)
+{
     // init I2C interface
+    int re;
     re = wiringPiI2CSetup(SSD1306_I2C_ADDRESS);
     if (re == -1) {
         printf("I2C Interface Setup failed.");
--- c-rasp/oled-ssd1306/src/ssd1306_i2c.h
+++ c-rasp/oled-ssd1306/src/ssd1306_i2c.h
@@ -8,20 +8,45 @@
 #ifndef SSD1306_I2C_H
 #define SSD1306_I2C_H
 
+// --- Define functions ---
+// ----------------------------------------------
+// init I2C interface and SSD1306
+void ssd1306I2CSetup(void);
+
+// send command byte to SSD1306
+// use unsigned char to express a byte of data
+void sendCommand(unsigned char command);
+// send data byte to SSD1306
+void sendChar(unsigned char data);
+
+// OLED settings
+void setCursorXY(int row, int col);
+void sendStrXY(const char *string, int X, int Y);
+
+// display operations
+void displayOn(void);
+void displayOff(void);
+void clearDisplay(void);
+
+// ----------------------------------------------
 
 // --- Define constants ----
 // infos can be found in SSD1306 datasheet
 // ----------------------------------------------
+// address
 #define SSD1306_I2C_ADDRESS 0x3C
+
+// commands
 #define SSD1306_DISPLAYON 0xAF
 #define SSD1306_DISPLAYOFF 0xAE
 #define SSD1306_NORMALDISPLAY 0xA6
 #define SSD1306_INVERTDISPLAY 0xA7
 
-#define SSD1306_CONTROL_REG 0x80
+// registers
+#define SSD1306_CONTROL_REG 0x00
 #define SSD1306_DATA_REG 0x40
 
-// Scrolling constants
+// scrolling constants
 #define SSD1306_ACTIVATE_SCROLL 0x2F
 #define SSD1306_DEACTIVATE_SCROLL 0x2E
 #define SSD1306_SET_VERTICAL_SCROLL_AREA 0xA3
@@ -29,22 +54,6 @@
 #define SSD1306_LEFT_HORIZONTAL_SCROLL 0x27
 #define SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29
 #define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A
-// ----------------------------------------------
-
-
-// --- Define functions ---
-// ----------------------------------------------
-// init SSD1306 Driver
-int ssd1306I2CSetup(void);
-
-// send commands byte
-void sendCommand(unsigned char command);
-
-// send char byte
-void sendChar(unsigned char data);
-
-void displayOn(void);
-void displayOff(void);
 // ----------------------------------------------
 
 #endif /* SSD1306_I2C_H */
