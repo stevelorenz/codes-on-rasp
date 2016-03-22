9103da65ceba8101a9d4a45fac56b0991ac918c6
Zuo Xiang <xianglinks@gmail.com>
2016-03-22 11:28:05 +0100

    finish SSD1306 with I2C interface driver programm in c

--- c-rasp/oled-ssd1306/edit.sh
+++ c-rasp/oled-ssd1306/edit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nvim ../oled-ssd1306/mozgy-ssd1306/OLED_SSD1306.cpp ../oled-ssd1306/mozgy-ssd1306/OLED_SSD1306.h ./src/ssd1306_i2c.c ./src/ssd1306_i2c.h ~/src/codes-on-rasp/py-rasp/oled-display/Adafruit_SSD1306/SSD1306.py
+nvim ../oled-ssd1306/mozgy-ssd1306/OLED_SSD1306.cpp ../oled-ssd1306/mozgy-ssd1306/OLED_SSD1306.h ./src/ssd1306_i2c.c ./src/ssd1306_i2c.h ~/src/codes-on-rasp/py-rasp/oled-display/Adafruit_SSD1306/SSD1306.py  ./ref_code/oled.c
--- c-rasp/oled-ssd1306/src/test_oled.c
+++ c-rasp/oled-ssd1306/src/test_oled.c
@@ -8,10 +8,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 #include "ssd1306_i2c.h"
 
 // --- main function ---
 int main(void) {
-    int fd = ssd1306I2CSetup();
+    int fd = ssd1306I2CSetup(0x3C);
+	draw_line(1, 1, "tmp is 20 C");
+	updateDisplay(fd);
+    sleep(2);
+    clearDisplay(fd);
+    sleep(2);
+	draw_line(1, 1, "tmp is 20 C");
+	updateDisplay(fd);
+    displayOff(fd);
     return 0;
 }
